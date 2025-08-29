982a54bc5a469e198c79de6c2f9468b4211f2883
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-29 12:08:02 +0900

    11. 정렬, 검색 필터링 만들기

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -5,28 +5,47 @@ import {useSearchParams} from 'react-router-dom';
 
 const BlogFilter = () => {
 
-    // 쿼리 스트링 생성 기능
-    const [seartchParams, setSearchParams] = useSearchParams();
+    // 쿼리스트링 생성 기능
+    const [searchParams, setSearchParams] = useSearchParams();
 
     // 카테고리 옵션 선택 이벤트
-    const handleCategoryChange = (e) => {
-        // console.log('카테고리가 선택됨 : ', e.target.value);
+    const handleCategoryChange = e => {
+        // console.log('category가 선택됨: ', e.target.value);
         // 쿼리스트링으로 ?category=xxx
-        setSearchParams(prev =>{
-            console.log(`prev: `, prev);
+        setSearchParams(prev => {
+            // console.log('prev: ', prev);
             prev.set('category', e.target.value);
             return prev;
         });
-    }
+    };
+
+    const handleSortChange = e => {
+        setSearchParams(prev => {
+            prev.set('sort', e.target.value);
+            return prev;
+        });
+    };
+
+    const handleSearch = e => {
+        setSearchParams(prev => {
+            prev.set('search', e.target.value);
+            return prev;
+        });
+    };
 
     return (
         <div className={styles.filter}>
-            <select onChange={handleCategoryChange} value={seartchParams.get('category') || 'all'}>
-                {categories.map(category =>
-                    (<option key={category.id} value={category.id}>{category.name}</option>))}
+            <select
+                onChange={handleCategoryChange}
+                value={searchParams.get('category') || 'all'}
+            >
+                {categories.map(ct => <option key={ct.id} value={ct.id}>{ct.name}</option>)}
             </select>
 
-            <select>
+            <select
+                onChange={handleSortChange}
+                value={searchParams.get('sort') || 'latest'}
+            >
                 <option value='latest'>최신순</option>
                 <option value='oldest'>오래된순</option>
             </select>
@@ -34,6 +53,8 @@ const BlogFilter = () => {
             <input
                 type='text'
                 placeholder='검색어를 입력하세요'
+                onChange={handleSearch}
+                value={searchParams.get('search') || ''}
             />
         </div>
     );
