42a5d6a5f9e8814e9293e34b9104bc0441e6d671
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-29 10:34:16 +0900

    5. 커스텀 에러페이지 사용하기

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -4,11 +4,14 @@ import BlogPage from '../pages/BlogPage.jsx';
 import AboutPage from '../pages/AboutPage.jsx';
 import {createBrowserRouter} from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout.jsx';
+import ErrorPage from '../pages/ErrorPage.jsx';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout/>,
+        // custom error page 설정
+        errorElement: <ErrorPage/>,
         // children -> Layout의 Outlet 부분을 뭘로 바뀔지 설정
         children: [
             {
