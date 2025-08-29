fdff8d11650839ba7ccf73749ca377522cc174f2
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-29 11:47:44 +0900

    10. 카테고리 필터링 만들기

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './BlogFilter.module.scss';
+import {categories} from '../dummy-data/dummy-post.js';
+import {useSearchParams} from 'react-router-dom';
 
 const BlogFilter = () => {
+
+    // 쿼리 스트링 생성 기능
+    const [seartchParams, setSearchParams] = useSearchParams();
+
+    // 카테고리 옵션 선택 이벤트
+    const handleCategoryChange = (e) => {
+        // console.log('카테고리가 선택됨 : ', e.target.value);
+        // 쿼리스트링으로 ?category=xxx
+        setSearchParams(prev =>{
+            console.log(`prev: `, prev);
+            prev.set('category', e.target.value);
+            return prev;
+        });
+    }
+
     return (
         <div className={styles.filter}>
-            <select>
-                <option>리액트</option>
-                <option>노드JS</option>
+            <select onChange={handleCategoryChange} value={seartchParams.get('category') || 'all'}>
+                {categories.map(category =>
+                    (<option key={category.id} value={category.id}>{category.name}</option>))}
             </select>
 
             <select>
